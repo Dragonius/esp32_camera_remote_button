fa5cf4bc47528529cac861a01588bce9dae7e259
Jari Luukkonen <Dragonius@users.noreply.github.com>
2025-10-20 22:05:13 +0300

    Update esp32_remote_button.ino

--- esp32_remote_button.ino
+++ esp32_remote_button.ino
@@ -1,48 +1,134 @@
 #include <BleKeyboard.h>
+#include <Preferences.h>
 
-BleKeyboard bleKeyboard("Furry-remote", "Flammever", 100);
+Preferences preferences;
+BleKeyboard bleKeyboard("Furry-Remote", "Flammever", 100);
 
 const int buttonPin = 4;
+const int shutdownPin = 2;
 bool buttonPressed = false;
+bool wasConnected = false;
 unsigned long lastPressTime = 0;
-const unsigned long pressDelay = 500; // Minimum time between presses
+const unsigned long pressDelay = 500;
+unsigned long shutdownPressTime = 0;
+const unsigned long shutdownHoldTime = 3000;
+unsigned long lastReconnectAttempt = 0;
+const unsigned long reconnectInterval = 5000; // Try every 5 seconds
+
+// Stored Bluetooth data
+String lastConnectedDevice = "";
+bool shouldReconnect = false;
 
 void setup() {
-  //Serial.begin(115200);
-  pinMode(buttonPin, INPUT_PULLUP); // Use internal pull-up resistor
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(shutdownPin, INPUT_PULLUP);
   
-  //Serial.println("Starting ESP32 Camera Trigger");
-  //Serial.println("Waiting for BLE connection...");
-  bleKeyboard.end();   // Disconnect any existing BLE session
-  delay(1000);          // Small delay to let stack reset
-  bleKeyboard.begin(); // Start advertising
+  // Initialize preferences (like EEPROM but better)
+  preferences.begin("bluetooth", false);
+  
+  // Load last connected device from memory
+  lastConnectedDevice = preferences.getString("lastDevice", "");
+  shouldReconnect = preferences.getBool("shouldReconnect", false);
+  
+  // Start BLE
+  startBLE();
+}
+
+void startBLE() {
+  bleKeyboard.begin();
+  
+  // If we have a last device and should reconnect, start advertising
+  if (shouldReconnect && lastConnectedDevice != "") {
+    // BLE will automatically try to reconnect to the last device
+  }
 }
 
 void loop() {
-  if (bleKeyboard.isConnected()) {
-    // Read button state
+  bool isConnected = bleKeyboard.isConnected();
+  
+  // Handle connection state changes
+  if (isConnected && !wasConnected) {
+    // Just connected - save this device for future reconnection
+    saveConnectionInfo(true);
+    wasConnected = true;
+  }
+  
+  if (!isConnected && wasConnected) {
+    // Just disconnected
+    wasConnected = false;
+    lastReconnectAttempt = millis();
+  }
+  
+  // Auto-reconnect if we lost connection but should be connected
+  if (!isConnected && shouldReconnect) {
+    if (millis() - lastReconnectAttempt > reconnectInterval) {
+      // Restart BLE to try reconnecting
+      bleKeyboard.end();
+      delay(100);
+      bleKeyboard.begin();
+      lastReconnectAttempt = millis();
+    }
+  }
+  
+  // Check shutdown button
+  checkShutdownButton();
+  
+  // Take pictures when connected
+  if (isConnected) {
     bool buttonState = digitalRead(buttonPin);
     
-    // Button is pressed (LOW because of pull-up)
     if (buttonState == LOW && !buttonPressed) {
       if (millis() - lastPressTime > pressDelay) {
-        //Serial.println("Triggering camera with Volume Up");
         bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
         buttonPressed = true;
         lastPressTime = millis();
-
       }
     }
     
-    // Button is released
     if (buttonState == HIGH && buttonPressed) {
-      bleKeyboard.release(KEY_MEDIA_VOLUME_UP);
       buttonPressed = false;
     }
-  } else {
-    //Serial.println("BLE not connected to phone");
-    delay(1000);
   }
   
   delay(50);
+}
+
+void saveConnectionInfo(bool connected) {
+  if (connected) {
+    // When connected, remember we should reconnect
+    preferences.putBool("shouldReconnect", true);
+    // Note: We can't easily get the actual device MAC address with BleKeyboard
+    // but we set the flag to attempt reconnection
+  } else {
+    // When manually shutdown, don't reconnect
+    preferences.putBool("shouldReconnect", false);
+  }
+  preferences.end();
+}
+
+void checkShutdownButton() {
+  if (digitalRead(shutdownPin) == LOW) {
+    if (shutdownPressTime == 0) {
+      shutdownPressTime = millis();
+    }
+    
+    if (millis() - shutdownPressTime >= shutdownHoldTime) {
+      shutdownDevice();
+    }
+  } else {
+    shutdownPressTime = 0;
+  }
+}
+
+void shutdownDevice() {
+  // Save that we're manually disconnecting (don't auto-reconnect)
+  saveConnectionInfo(false);
+  
+  // Disconnect Bluetooth
+  bleKeyboard.end();
+  
+  // Stop operation
+  while(true) {
+    delay(1000);
+  }
 }
